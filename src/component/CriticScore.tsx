560a4a5f25d510dd2d6b31ff2daf8e5e258bd426
nurezf <nuredinzf1996@gmail.com>
2024-10-12 03:38:22 -0700

    displaying critic score

---
+++ src/component/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  // Adjust the color based on the score value
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "red";
+  return (
+    <Badge borderRadius="4px" paddingX={2} fontSize="14px" colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;

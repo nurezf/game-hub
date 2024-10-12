8ba800cb53cac6a13cb652bbd5921c7821dce713
nurezf <nuredinzf1996@gmail.com>
2024-10-12 16:40:01 -0700

    display genres

--- src/component/GenresList.tsx
+++ src/component/GenresList.tsx
@@ -1,13 +1,24 @@
+import { HStack, List, ListItem, Text, Image } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../hooks/image-url";
 
 const GenresList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem paddingY="5px" key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
